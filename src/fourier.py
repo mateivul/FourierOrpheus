c7b49a93d763fa1e233043463d8bb29ab62e5796
mateivul <mateivultur08@gmail.com>
2026-03-28 09:06:05 +0200

    sorted DFT coeff computation

---
+++ src/fourier.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def coeffs(points, n_coeffs):
+    coefficients = np.fft.fft(points, norm="forward")
+    freqs = np.fft.fftfreq(len(points), 1 / len(points))
+
+    dc = np.where(freqs == 0)[0]
+    ac = np.where(freqs != 0)[0]
+    order = np.argsort(-np.abs(coefficients[ac]))
+    ac = ac[order[:n_corffs - len(dc)]]
+
+    freqs = freqs[keep]
+    coefficients = coefficients[keep]
+    amps = np.abs(coefficients)
+    phases = np.angle(coefficients)
+
+    return amps, freqs, phases 
